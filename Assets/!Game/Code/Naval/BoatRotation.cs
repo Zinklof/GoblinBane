6cc0e92555d6f23e1130aa99b2e2da44d787f024
Zinklof <123103261+Zinklof@users.noreply.github.com>
2024-04-19 13:12:13 -0400

    further cutscene work

---
+++ Assets/!Game/Code/Naval/BoatRotation.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class BoatRotation : MonoBehaviour
+{
+    [SerializeField] float maxChange;
+    [SerializeField] float maxChangePerTick;
+
+    [SerializeField] Vector2 currentRotation;
+    [SerializeField] Vector2 rotation;
+
+    [SerializeField] float tickrate;
+
+    [SerializeField] bool DoThisScript;
+
+    private void Start()
+    {
+        StartCoroutine(Rotator());
+    }
+
+    private IEnumerator Rotator()
+    {
+        while (DoThisScript = true)
+        {
+            rotation.x += Random.Range(-maxChangePerTick, maxChangePerTick);
+            rotation.y += Random.Range(-maxChangePerTick, maxChangePerTick);
+
+            rotation.x = Mathf.Clamp(rotation.x, -maxChange, maxChange);
+            rotation.y = Mathf.Clamp(rotation.y, -maxChange, maxChange);
+
+            yield return new WaitForSeconds(tickrate);
+        }
+    }
+
+    private void Update()
+    {
+        currentRotation.x = Mathf.Lerp(currentRotation.x, rotation.x, 0.0025f);
+
+        currentRotation.y = Mathf.Lerp(currentRotation.y, rotation.y, 0.0025f);
+
+        Vector3 finalRoation = new Vector3(currentRotation.x, transform.rotation.y, currentRotation.y);
+
+        transform.rotation = Quaternion.Euler(finalRoation);
+    }
+}
